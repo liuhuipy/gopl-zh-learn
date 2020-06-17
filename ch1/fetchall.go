30f24f81910f63228b177f60916a6dfbe5143e88
liuhuipy <liuhui_py@163.com>
2020-06-17 21:04:12 +0800

    Update ch1 fetchall goroutine example

--- ch1/fetchall.go
+++ ch1/fetchall.go
@@ -9,6 +9,20 @@ import (
 	"time"
 )
 
+/*
+goroutine是一种函数的并发执行方式，而channel是用来在goroutine之间进行参数传递。main函数本身也运行在一个goroutine中，而go function则表示
+创建了一个新的goroutine，并在这个新的goroutine中执行这个函数。
+
+main函数中用make函数创建了一个传递string类型参数的channel，对每一个命令行参数，我们都用go这个关键字来创建一个goroutine，并且让函数在这个
+goroutine异步执行http.Get方法。这个程序里的io.Copy会把响应的Body内容拷贝到ioutil.Discard输出流中，因为我们需要这个方法返回的字节数，但是
+又不想要其内容。每当请求返回内容时，fetch函数都会往ch这个channel里写入一个字符串，由main函数里的第二个for循环来处理并打印channel里的字符串。
+
+当一个goroutine尝试在一个channel上做send或者receive操作时，这个goroutine会阻塞在调用处，知道另一个goroutine往这个channel里写入或者接收
+值，这样两个goroutine才会继续执行channel操作之后的逻辑。这个例子中，每一个fetch函数在执行时都会往channel里发送一个值(ch<-expression)，
+可以避免在goroutine异步执行还没有完成时main函数提前退出。
+
+*/
+
 func main() {
 	startTime := time.Now()
 	ch := make(chan string)
